b3181c94bafd7d47e60eafb6443503a44d0bb26f
César Augusto <96089473+CesarAugusto-mm@users.noreply.github.com>
2023-10-17 00:08:44 -0300

    Add files via upload

---
+++ produtoEstoque-1/src/main/java/com/redeSocial/ProdutoEstoque1Application.java
@@ -0,0 +1,13 @@
+package com.redeSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoEstoque1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoEstoque1Application.class, args);
+	}
+
+}
